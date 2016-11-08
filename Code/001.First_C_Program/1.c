f7cf430eff31ac826340e780b8f04fcfc9175a0c
Fujie Chen <imaple7@gmail.com>
2016-11-08 01:34:34 -0600

    Translate 001 file

---
+++ Code/001.First_C_Program/1.c
@@ -0,0 +1,6 @@
+/* The first C program */
+#include <stdio.h>				/* Inclde standard io header file */
+main()						/* Main function */
+{
+		printf("Hello World!\n");	/* Print out the information */
+}
